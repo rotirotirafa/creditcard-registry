eb9ea801689abe3260449bce6a22830a9d3d4c7e
rafael rotiroti <rotirotirafa@gmail.com>
2023-11-18 17:36:03 -0300

    fix: new folder structure

--- api/src/main.py
+++ api/src/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
 
-from src.entrypoints.routes.v1 import api_router
+# from src.entrypoints.routes.v1 import api_router
 
 
 app = FastAPI()
 
-app.include_router(api_router)
+# app.include_router(api_router)
 
 
 @app.get("/")
